9384625e8281f6758ab1e24e3b8b67e87ca307aa
Jeepers009 <jeepers009@gmail.com>
2023-08-28 16:56:04 +0300

    userDao changed

--- src/main/java/by/itclass/model/dao/user/UserDao.java
+++ src/main/java/by/itclass/model/dao/user/UserDao.java
@@ -43,18 +43,24 @@ public class UserDao {
     public boolean addUser(User user, String password) {
         try (Connection cn = ConnectionManager.getConnection();
              PreparedStatement ps = cn.prepareStatement(INSERT_USER)) {
-
+            if (isAccessible(user.getLogin())) {
+                ps.setString(1, user.getLogin());
+                ps.setString(2, user.getName());
+                ps.setString(3, password);
+                ps.setString(4, user.getEmail());
+                return ps.executeUpdate() > 0;
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
     }
-
+    
     private boolean isAccessible(String login) {
-            try ( Connection cn = ConnectionManager.getConnection();
-                  PreparedStatement ps = cn.prepareStatement(SELECT_USER_BY_LOGIN)) {
-                ps.setString(1, login);
-                return !ps.executeQuery().next();
+        try (Connection cn = ConnectionManager.getConnection();
+             PreparedStatement ps = cn.prepareStatement(SELECT_USER_BY_LOGIN)) {
+            ps.setString(1, login);
+            return !ps.executeQuery().next();
         } catch (SQLException e) {
             e.printStackTrace();
         }
